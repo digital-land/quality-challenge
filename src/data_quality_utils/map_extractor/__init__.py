a7d0b11c575de8e2d7f17e495c492bd63336ed13
Simona Lihosithova <simona.lihosithova@faculty.ai>
2025-04-22 10:56:40 +0100

    Refactor imports

--- src/data_quality_utils/map_extractor/__init__.py
+++ src/data_quality_utils/map_extractor/__init__.py
@@ -1,5 +1,5 @@
-from .google_static_maps_extractor import GoogleStaticMapsExtractor
 from .google_map_tiles_extractor import GoogleMapTilesExtractor
+from .google_static_maps_extractor import GoogleStaticMapsExtractor
 from .wms_extractor import WMSExtractor
 
 __all__ = ["GoogleMapTilesExtractor", "GoogleStaticMapsExtractor", "WMSExtractor"]
