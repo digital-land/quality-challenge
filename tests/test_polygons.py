e139d7fdebe05a09031c861cc2313c52410949a6
Oliver Newcombe <oliver.newcombe2@faculty.ai>
2025-04-28 10:04:50 +0100

    Added pytest fixtures to testing functions.

--- tests/test_polygons.py
+++ tests/test_polygons.py
@@ -1,7 +1,11 @@
+import pytest
 from geopandas import GeoDataFrame
 from shapely import Polygon, unary_union
 
 from data_quality_utils.polygon.polygon_matcher import PolygonMatcher
+
+BASE_CRS = "EPSG:4326"
+MERCATOR_CRS = "EPSG:3857"
 
 
 def create_square_coords(lat: int = 0, long: int = 0, add_buffer=0.01) -> Polygon:
@@ -23,29 +27,36 @@ def create_polygon_df(coords_list: list, crs: str) -> GeoDataFrame:
     return gdf
 
 
-def test_identical_polygons():
-    matcher = PolygonMatcher()
+@pytest.fixture
+def square_df() -> GeoDataFrame:
     base_list = create_square_coords()
-    base_df = create_polygon_df(base_list, matcher.base_crs)
-    features_list = create_square_coords()
-    features_df = create_polygon_df(features_list, matcher.base_crs)
-    features_series = features_df.buffer(0)
-    aligned_df, diff_df = matcher.match_polygon_to_features(base_df, features_series)
-    diff_area = diff_df.geometry[0].area
-    aligned_area = aligned_df.geometry[0].area
-
-    assert diff_area == 0.0
-    assert base_df.to_crs(matcher.mercator_crs).geometry[0].area == aligned_area
+    return create_polygon_df(base_list, BASE_CRS)
 
 
-def test_overlapping_polygons():
-    matcher = PolygonMatcher(polygon_snap_distance=100)
-    lat, long = (0, 0)
-    base_list = create_square_coords(lat, long, add_buffer=0.001)
-    base_df = create_polygon_df(base_list, matcher.base_crs)
-    features_df = create_polygon_df(
+@pytest.fixture
+def small_square_df(add_buffer=0.001) -> GeoDataFrame:
+    base_list = create_square_coords(add_buffer=add_buffer)
+    return create_polygon_df(base_list, BASE_CRS)
+
+
+@pytest.fixture
+def rectangle_df(long: float = 0, lat: float = 0):
+    return create_polygon_df(
         [
-            (long - 0.0001 + add[0], lat + add[1])
+            (long + add[0], lat + add[1])
+            for add in [(0.001, 0.001), (0.001, -0.001), (0, -0.001), (0, 0.001)]
+        ],
+        BASE_CRS,
+    )
+
+
+@pytest.fixture
+def shifted_pentagon_df(
+    long: float = 0, lat: float = 0, shift: float = 0.0001
+) -> GeoDataFrame:
+    return create_polygon_df(
+        [
+            (long - shift + add[0], lat + add[1])
             for add in [
                 (0.001, 0.001),
                 (0.0015, 0),
@@ -54,110 +65,13 @@ def test_overlapping_polygons():
                 (0.0009, 0.001),
             ]
         ],
-        matcher.base_crs,
-    )
-    features_df = features_df.to_crs(matcher.mercator_crs)
-    features_series = features_df.buffer(0)
-    aligned_df, diff_df = matcher.match_polygon_to_features(base_df, features_series)
-    diff_area = diff_df.geometry[0].area
-    aligned_area = aligned_df.geometry[0].area
-
-    assert diff_area is not None
-    assert aligned_area is not None
-    assert diff_area > 0.0
-    assert aligned_area > 0.0
-
-
-def test_large_areas_list():
-    matcher = PolygonMatcher()
-    lat, long = (0, 0)
-    aligned_df = create_polygon_df(
-        [
-            (long + add[0], lat + add[1])
-            for add in [
-                (0.001, 0.001),
-                (0.0015, 0),
-                (0.001, -0.001),
-                (0, -0.001),
-                (0, 0.001),
-            ]
-        ],
-        matcher.base_crs,
-    )
-    diff_df = create_polygon_df(
-        [
-            (long + add[0], lat + add[1])
-            for add in [(0.001, 0.001), (0.0015, 0), (0.001, -0.001)]
-        ],
-        matcher.base_crs,
-    )
-    aligned_df = aligned_df.to_crs(matcher.mercator_crs)
-    diff_df = diff_df.to_crs(matcher.mercator_crs)
-    aligned_series = aligned_df.buffer(0)
-    large_area_list = matcher.calculate_area_of_large_discrepancies(
-        aligned_series, diff_df
-    )
-    large_area_empty_list = matcher.calculate_area_of_large_discrepancies(
-        aligned_series, diff_df, area_threshold=2 * aligned_df.area[0]
+        BASE_CRS,
     )
 
-    assert isinstance(large_area_list, list)
-    assert diff_df.area[0] in large_area_list
-    assert not large_area_empty_list
 
-
-def test_large_areas_total():
-    matcher = PolygonMatcher()
-    lat, long = (0, 0)
-    aligned_df = create_polygon_df(
-        [
-            (long + add[0], lat + add[1])
-            for add in [
-                (0.001, 0.001),
-                (0.0015, 0),
-                (0.001, -0.001),
-                (0, -0.001),
-                (0, 0.001),
-            ]
-        ],
-        matcher.base_crs,
-    )
-    diff_df = create_polygon_df(
-        [
-            (long + add[0], lat + add[1])
-            for add in [(0.001, 0.001), (0.0015, 0), (0.001, -0.001)]
-        ],
-        matcher.base_crs,
-    )
-    aligned_df = aligned_df.to_crs(matcher.mercator_crs)
-    diff_df = diff_df.to_crs(matcher.mercator_crs)
-    aligned_series = aligned_df.buffer(0)
-    large_area_total = matcher.calculate_total_area_of_discrepancies(
-        aligned_series, diff_df
-    )
-
-    assert isinstance(large_area_total, float)
-    assert sum(diff_df.area) == large_area_total
-
-
-def test_area_proportions():
-    matcher = PolygonMatcher()
-    lat, long = (0, 0)
-    base_df = create_polygon_df(
-        [
-            (long + add[0], lat + add[1])
-            for add in [(0.001, 0.001), (0.001, -0.001), (0, -0.001), (0, 0.001)]
-        ],
-        matcher.base_crs,
-    )
-    diff_df = create_polygon_df(
-        [
-            (long + add[0], lat + add[1])
-            for add in [(0.001, 0.001), (0.0015, 0), (0.001, -0.001)]
-        ],
-        matcher.base_crs,
-    )
-    features_df = create_polygon_df(
+@pytest.fixture
+def shorter_pentagon_df(long: float = 0, lat: float = 0) -> GeoDataFrame:
+    return create_polygon_df(
         [
             (long + add[0], lat + add[1])
             for add in [
@@ -168,8 +82,118 @@ def test_area_proportions():
                 (0.0005, 0.001),
             ]
         ],
-        matcher.base_crs,
+        BASE_CRS,
     )
+
+
+@pytest.fixture
+def pentagon_df(long: float = 0, lat: float = 0) -> GeoDataFrame:
+    return create_polygon_df(
+        [
+            (long + add[0], lat + add[1])
+            for add in [
+                (0.001, 0.001),
+                (0.0015, 0),
+                (0.001, -0.001),
+                (0, -0.001),
+                (0, 0.001),
+            ]
+        ],
+        BASE_CRS,
+    )
+
+
+@pytest.fixture
+def triangle_df(long: float = 0, lat: float = 0) -> GeoDataFrame:
+    return create_polygon_df(
+        [
+            (long + add[0], lat + add[1])
+            for add in [(0.001, 0.001), (0.0015, 0), (0.001, -0.001)]
+        ],
+        BASE_CRS,
+    )
+
+
+def test_identical_polygons(square_df: GeoDataFrame):
+    matcher = PolygonMatcher()
+    base_df = square_df.copy()
+    features_df = square_df.copy()
+    features_series = features_df.buffer(0)
+    aligned_df, diff_df = matcher.match_polygon_to_features(base_df, features_series)
+    diff_area = diff_df.geometry[0].area
+
+    assert diff_area == 0.0
+
+
+def test_overlapping_polygons(
+    small_square_df: GeoDataFrame, shifted_pentagon_df: GeoDataFrame
+):
+    matcher = PolygonMatcher(polygon_snap_distance=100)
+    base_df = small_square_df.copy()
+    features_df = shifted_pentagon_df.copy()
+    features_df = features_df.to_crs(matcher.mercator_crs)
+    features_series = features_df.buffer(0)
+    aligned_df, diff_df = matcher.match_polygon_to_features(base_df, features_series)
+    diff_area = diff_df.geometry[0].area
+    aligned_area = aligned_df.geometry[0].area
+
+    assert diff_area > 0.0
+    assert aligned_area > 0.0
+
+
+def test_large_areas_list_calculations(
+    pentagon_df: GeoDataFrame, triangle_df: GeoDataFrame
+):
+    matcher = PolygonMatcher()
+    aligned_df = pentagon_df.copy()
+    diff_df = triangle_df.copy()
+    aligned_df = aligned_df.to_crs(matcher.mercator_crs)
+    diff_df = diff_df.to_crs(matcher.mercator_crs)
+    aligned_series = aligned_df.buffer(0)
+    large_area_list = matcher.calculate_area_of_large_discrepancies(
+        aligned_series, diff_df
+    )
+
+    assert diff_df.area[0] in large_area_list
+
+
+def test_large_areas_empty_list(pentagon_df: GeoDataFrame, triangle_df: GeoDataFrame):
+    matcher = PolygonMatcher()
+    aligned_df = pentagon_df.copy()
+    diff_df = triangle_df.copy()
+    aligned_df = aligned_df.to_crs(matcher.mercator_crs)
+    diff_df = diff_df.to_crs(matcher.mercator_crs)
+    aligned_series = aligned_df.buffer(0)
+    large_area_empty_list = matcher.calculate_area_of_large_discrepancies(
+        aligned_series, diff_df, area_threshold=2 * aligned_df.area[0]
+    )
+
+    assert not large_area_empty_list
+
+
+def test_large_areas_total(pentagon_df: GeoDataFrame, triangle_df: GeoDataFrame):
+    matcher = PolygonMatcher()
+    aligned_df = pentagon_df.copy()
+    diff_df = triangle_df.copy()
+    aligned_df = aligned_df.to_crs(matcher.mercator_crs)
+    diff_df = diff_df.to_crs(matcher.mercator_crs)
+    aligned_series = aligned_df.buffer(0)
+    large_area_total = matcher.calculate_total_area_of_discrepancies(
+        aligned_series, diff_df
+    )
+
+    assert sum(diff_df.area) == large_area_total
+
+
+def test_area_proportions(
+    rectangle_df: GeoDataFrame,
+    triangle_df: GeoDataFrame,
+    shorter_pentagon_df: GeoDataFrame,
+):
+    matcher = PolygonMatcher()
+    base_df = rectangle_df.copy()
+    diff_df = triangle_df.copy()
+    features_df = shorter_pentagon_df.copy()
     aligned_poly = unary_union([base_df.geometry, features_df.geometry])
     aligned_df = GeoDataFrame([1], geometry=[aligned_poly], crs=matcher.base_crs)
     diff_df = diff_df.to_crs(matcher.mercator_crs)
@@ -180,5 +204,4 @@ def test_area_proportions():
         features_series, aligned_df, diff_df
     )
 
-    assert isinstance(area_proportion, float)
     assert area_proportion == 100 * sum(diff_df.area) / sum(aligned_df.area)
